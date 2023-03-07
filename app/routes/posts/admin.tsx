7b151987a248e6a52828ae285b02a2a053ed77ea
wangbc2 <wangbc2@gmail.com>
2023-03-07 12:46:07 +0900

    new - useTransition delete - useFetcher 등

--- app/routes/posts/admin.tsx
+++ app/routes/posts/admin.tsx
@@ -1,6 +1,13 @@
-import { json } from "@remix-run/node";
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/models/post.server";
+import { ActionArgs, json } from "@remix-run/node";
+import {
+  Form,
+  Link,
+  Outlet,
+  useFetcher,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
+import { deletePost, getPosts, Post } from "~/models/post.server";
 
 export const loader = async () => {
   console.log("loader gogo");
@@ -8,6 +15,18 @@ export const loader = async () => {
   return json({
     posts: await getPosts(),
   });
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  console.log(Object.fromEntries(formData));
+
+  const { id } = Object.fromEntries(formData);
+
+  await new Promise((res) => setTimeout(res, 1000));
+  await deletePost(Number(id));
+
+  return "./";
 };
 
 const Posts = () => {
@@ -19,17 +38,40 @@ const Posts = () => {
     <div>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={`update/${post.id}`}>
-              <span>{post.title}</span>
-            </Link>
-          </li>
+          <PostItem post={post} />
         ))}
       </ul>
       <ul>
         <Outlet />
       </ul>
     </div>
+  );
+};
+
+const PostItem = ({ post }: { post: Post }) => {
+  // const transition = useTransition();
+  const fetcher = useFetcher();
+  const isDeleting = Boolean(
+    Number(fetcher.submission?.formData.get("id")) === post.id
+  );
+
+  return (
+    <li
+      style={{
+        opacity: isDeleting ? 0.25 : 1,
+      }}
+      key={post.id}
+    >
+      <Link to={`update/${post.id}`}>
+        <span>{post.title}</span>
+      </Link>
+      <fetcher.Form method="post">
+        <input type="hidden" name="id" value={post.id} />
+        <button type="submit" name="_action" value="delete">
+          삭제
+        </button>
+      </fetcher.Form>
+    </li>
   );
 };
 
--- app/routes/posts/admin/new/index.tsx
+++ app/routes/posts/admin/new/index.tsx
@@ -1,5 +1,11 @@
 import { ActionArgs, redirect } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import {
+  Form,
+  useNavigate,
+  useNavigation,
+  useTransition,
+} from "@remix-run/react";
+import { useEffect, useRef } from "react";
 import { createPost } from "~/models/post.server";
 
 export const action = async ({ request }: ActionArgs) => {
@@ -8,19 +14,37 @@ export const action = async ({ request }: ActionArgs) => {
   const title = formData.get("title");
   const content = formData.get("content");
 
+  await new Promise((res) => setTimeout(res, 1000));
+
   await createPost({ title, content, authorId: 1 } as any);
 
-  return redirect("/posts/admin");
+  // return redirect("/posts/admin");
+
+  return true;
 };
 
 export default function New() {
+  // const navigation = useNavigation();
+  const transition = useTransition();
+  const isCreating = Boolean(transition.state === "submitting");
+
+  const formRef = useRef<HTMLFormElement>(null);
+  const titleRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!isCreating) {
+      formRef.current?.reset();
+      titleRef.current?.focus();
+    }
+  }, [isCreating]);
+
   return (
     <div>
-      <Form method="post">
+      <Form ref={formRef} method="post">
         <p>
           <label>
             <span>title</span>
-            <input type="text" name="title" />
+            <input ref={titleRef} type="text" name="title" />
           </label>
         </p>
         <p>
@@ -30,7 +54,7 @@ export default function New() {
           </label>
         </p>
         <p>
-          <button type="submit">등록</button>
+          <button type="submit">{isCreating ? "등록중" : "등록"}</button>
         </p>
       </Form>
     </div>
